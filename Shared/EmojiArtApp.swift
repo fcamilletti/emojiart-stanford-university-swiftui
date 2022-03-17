db4ee8838905276af72377ec8e17f13ad2cdfe93
Fran <franco.camilletti.09@gmail.com>
2022-03-17 16:07:49 -0300

    Creating the Model: Background and Emojis structs and Properties

---
+++ Shared/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  Shared
+//
+//  Created by Franco Camilletti on 17/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
