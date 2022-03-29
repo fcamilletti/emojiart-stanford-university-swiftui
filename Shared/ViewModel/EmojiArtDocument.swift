f53d761631d10fe1f65f122b3777c669d668eba3
Fran <franco.camilletti.09@gmail.com>
2022-03-28 21:24:12 -0300

    Created all the functions to recognize and adapt different images and types of urls using drag and drop to change the background, Applied some utility extensions and utility views to check.

--- Shared/ViewModel/EmojiArtDocument.swift
+++ Shared/ViewModel/EmojiArtDocument.swift
@@ -9,7 +9,13 @@ import SwiftUI
 
 class EmojiArtDocument: ObservableObject {
     
-    @Published private(set) var emojiArt: EmojiArtModel
+    @Published private(set) var emojiArt: EmojiArtModel {
+        didSet {
+            if emojiArt.background != oldValue.background {
+                fetchBackgroundImageDataIfNecessary()
+            }
+        }
+    }
     
     init() {
         emojiArt = EmojiArtModel()
@@ -19,6 +25,24 @@ class EmojiArtDocument: ObservableObject {
     
     var emojis: [EmojiArtModel.Emoji] { emojiArt.emojis }
     var background: EmojiArtModel.Background { emojiArt.background }
+    
+    @Published var backgroundImage: UIImage?
+    
+    private func fetchBackgroundImageDataIfNecessary() {
+        backgroundImage = nil
+        switch emojiArt.background {
+        case .url(let url):
+            let imageData = try? Data(contentsOf: url)
+            if imageData != nil {
+                backgroundImage = UIImage(data: imageData!)
+            }
+        case .imageData(let data):
+            backgroundImage = UIImage(data: data)
+        case .blank:
+            break
+            
+        }
+    }
     
     // MARK: - Intents
     
